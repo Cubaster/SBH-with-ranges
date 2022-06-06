8f62987a047a1bcafee3674f1186a59d4a8f22f8
Cubaster <jakub.dun1@gmail.com>
2022-06-06 14:02:22 +0200

    Update

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 
 def customDistance(oligo1: str, oligo2: str):
-    # return index no. at which oligo1 whole string\string end is identical with oligo2 whole string\substring beginning
+    # return index no. at which oligo1 substring is identical with oligo2 substring - further interpreted as weight
     # strings has to be equal in length
 
     rowSize = len(oligo1)
